ede3db752c5f3a5d53d7307b2eddcd1b7eacb550
Mazi Muhlari <mazi.muhlari@gmail.com>
2024-11-26 18:53:13 +0200

    updates for buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,107 @@
 import {Text, View, StyleSheet} from "react-native";
-import { Stack, Link } from "expo-router";
+import {Stack, Link} from "expo-router";
+import {useState} from "react";
 
 import Control from "@/components/Control";
-import Button from "@/components/Button";
 
 export default function Index() {
+    const [mix, setMix] = useState<number>(30);
+    const [fuel, setFuel] = useState<number>(0);
+    const [ethanol, setEthanol] = useState<number>(0);
+    const [contents, setContents] = useState<string>("0");
+
+    const onMoreMix = () => {
+        if (mix < 100)
+            setMix(mix + 5);
+
+        setEthanol(0);
+        setFuel(0);
+        setContents("0");
+    }
+
+    const onLessMix = () => {
+        if (mix > 0)
+            setMix(mix - 5);
+
+        setEthanol(0);
+        setFuel(0);
+        setContents("0");
+    }
+    const onMoreFuel = () => {
+        const ethanolProportion = (mix / 100);
+        const fuelProportion = (1 - (mix / 100));
+
+        const uFuel = fuel + 1;
+        const uEthanol = ((ethanolProportion / fuelProportion) * uFuel);
+        const uContents = uEthanol + uFuel;
+
+        setFuel(uFuel);
+        setEthanol(uEthanol);
+        setContents(uContents.toFixed(1));
+    }
+    const onLessFuel = () => {
+        const ethanolProportion = (mix / 100);
+        const fuelProportion = (1 - (mix / 100));
+
+        const uFuel = fuel > 0 ? fuel - 1 : fuel;
+        const uEthanol = (ethanolProportion / fuelProportion) * uFuel;
+        const uContents = uEthanol + uFuel;
+
+        setFuel(uFuel);
+        setEthanol(uEthanol);
+        setContents(uContents.toFixed(1));
+    }
+    const onMoreEthanol = () => {
+        const fuelProportion = (1 - (mix / 100));
+        const ethanolProportion = (mix / 100);
+
+        const uEthanol = ethanol + 1;
+        const uFuel = (fuelProportion / ethanolProportion) * uEthanol;
+        const uContents = uFuel + uEthanol;
+
+        setFuel(uFuel);
+        setEthanol(uEthanol);
+        setContents(uContents.toFixed(1));
+    }
+    const onLessEthanol = () => {
+        const fuelProportion = (1 - (mix / 100));
+        const ethanolProportion = (mix / 100);
+
+        const uEthanol = ethanol > 0 ? ethanol - 1 : ethanol;
+        const uFuel = (fuelProportion / ethanolProportion) * uEthanol;
+        const uContents = uFuel + uEthanol;
+
+        setFuel(uFuel);
+        setEthanol(uEthanol);
+        setContents(uContents.toFixed(1));
+    }
+
+    const onShareMix = () => {
+        // await Share.RequestAsync(new ShareTextRequest
+        // {
+        //     Title = "Share Ethanol Mix",
+        //         Subject = "Share Ethanol Mix",
+        //         Text = string.Format("Hi, I just used the 'Happymods Ethanol Calculator ethanol calculator' to calculate my latest ethanol and fuel mixture.\n\nMix: E{0}\nFuel: {1}l\nEthanol: {2}l\n\nTotal Content: {3}l\n\nYou can also calculate your own ethanol fuel mixtures by downloading the app {4}", Mix, Math.Round(Fuel, 1), Math.Round(Ethanol, 1), Math.Round(TotalContent, 1), "http://onelink.to/9z7pkt")
+        // });
+    }
+
     return (
         <>
-            <Stack.Screen options={{title: 'HAPPYMODS' }} />
+            <Stack.Screen options={{title: 'HAPPYMODS'}}/>
             <View style={styles.container}>
                 <View style={styles.controlsContainer}>
-                    <Control title="Mix" unit="E" />
-                    <Control title="Fuel" unit="E" />
-                    <Control title="Ethanol" unit="E" />
+                    <Control title="Mix" value={mix} prefix="E" onButtonLeftPress={onLessMix} onButtonRightPress={onMoreMix} />
+                    <Control title="Fuel" value={fuel} suffix="l" onButtonLeftPress={onLessFuel} onButtonRightPress={onMoreFuel} />
+                    <Control title="Ethanol" value={ethanol} suffix="l" onButtonLeftPress={onLessEthanol} onButtonRightPress={onMoreEthanol} />
                 </View>
                 <View style={styles.contentsContainer}>
-                    <Text style={styles.contentsValue}>54l</Text>
+                    <Text style={styles.contentsValue}>{contents}l</Text>
                     <Text style={styles.contentsTitle}>Total</Text>
                 </View>
                 <View style={styles.informationContainer}>
                     <Text style={styles.informationText}>
-                        This calculator does not take the quality of your fuel and ethanol or their contents into account
+                        This calculator does not take the quality of your fuel and ethanol or their contents into
+                        account
                         when calculating your fuel mix ratios.
                     </Text>
                 </View>
--- components/Control.tsx
+++ components/Control.tsx
@@ -3,21 +3,29 @@ import ControlButton from "@/components/ControlButton";
 
 type Props = {
     title: string;
-    unit: string;
+    value: number;
+    prefix?: string;
+    suffix?: string;
+    onButtonLeftPress: () => void;
+    onButtonRightPress: () => void;
+    // unit: string;
+    // increment: number;
 }
 
-export default function Control({ title, unit }: Props) {
+export default function Control({ title, value, prefix, suffix, onButtonLeftPress, onButtonRightPress }: Props) {
     return(
         <View style={styles.controlContainer}>
             <View style={styles.controlButtonLeft}>
-                <ControlButton icon="minus" />
+                <ControlButton icon="minus" onPress={onButtonLeftPress} />
             </View>
             <View style={styles.controlData}>
                 <Text style={styles.controlTitle}>{title}</Text>
-                <Text style={styles.controlValue}>0</Text>
+                <Text style={styles.controlValue}>{prefix}</Text>
+                <Text style={styles.controlValue}>{value}</Text>
+                <Text style={styles.controlValue}>{suffix}</Text>
             </View>
             <View style={styles.controlButtonRight}>
-                <ControlButton icon="plus" />
+                <ControlButton icon="plus" onPress={onButtonRightPress} />
             </View>
         </View>
     );
@@ -38,11 +46,10 @@ const styles = StyleSheet.create({
     controlTitle: {
         fontSize: 18,
         fontWeight: "900",
-        marginRight: 3,
+        marginRight: 6,
     },
     controlValue: {
         fontSize: 18,
-        marginLeft: 3,
     },
     controlButtonLeft: {
     },
